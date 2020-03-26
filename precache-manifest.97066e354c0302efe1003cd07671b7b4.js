9d5a7a66a2c6b4594e5edb413f665bc12a98646d
Szymon Bergner <szymon.bergner@gmail.com>
2020-03-26 12:51:04 +0100

    Updates

---
+++ precache-manifest.97066e354c0302efe1003cd07671b7b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc655fd342eae27502dc",
+    "url": "/react-app/static/css/main.3cc33853.chunk.css"
+  },
+  {
+    "revision": "dc655fd342eae27502dc",
+    "url": "/react-app/static/js/main.224e28df.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "7e22629cfd74b296b1e4",
+    "url": "/react-app/static/js/2.2db41df9.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "bb0fdf890b9626575ebda9f320176583",
+    "url": "/react-app/index.html"
+  }
+];
